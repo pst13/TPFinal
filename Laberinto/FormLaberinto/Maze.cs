34f0746552200547c44744b801bff1c9666cd78d
pst13 <raulm3425@outlook.com>
2017-06-21 20:16:50 -0300

    Laberinto.

---
+++ Laberinto/FormLaberinto/Maze.cs
@@ -0,0 +1,161 @@
+﻿using System;                                                               
+using System.Collections;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace FormLaberinto
+{
+    /// <summary>
+    /// Summary description for Maze.
+    /// </summary>
+    public class Maze
+    {
+        public static int kDimension = 10;
+        public Cell[,] Cells ;
+        Stack CellStack = new Stack();
+        int VisitedCells = 1;
+        int TotalCells = kDimension * kDimension;
+        Cell CurrentCell = null;
+        Random random;
+        public static Cell inicio;
+        public static Cell fin;
+
+                            
+        public Maze()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+            Initialize();
+        }
+
+        private ArrayList GetNeighborsWithWalls(Cell aCell)
+        {
+            ArrayList Neighbors = new ArrayList();
+            //otro arraylist con los vecinos
+
+            for (int countRow = -1; countRow <= 1; countRow++)
+                for (int countCol = -1; countCol <= 1; countCol++)
+                {
+                    if ((aCell.Row + countRow < kDimension) &&
+                         (aCell.Column + countCol < kDimension) &&
+                         (aCell.Row + countRow >= 0) &&
+                         (aCell.Column + countCol >= 0) &&
+                         ((countCol == 0) || (countRow == 0)) &&
+                         (countRow != countCol)
+                        )
+                    {
+                        if (Cells[aCell.Row + countRow, aCell.Column + countCol].HasAllWalls())
+                        {
+                            Neighbors.Add(Cells[aCell.Row + countRow, aCell.Column + countCol]);
+                            //agrega todos los vecinos que tienen todas las paredes
+                        }
+                    }
+                }
+
+            return Neighbors;
+        }
+
+        public void Initialize()
+        {
+            Cells = new Cell[kDimension, kDimension];
+            TotalCells = kDimension * kDimension;
+            //inicializo las celdas con su propiedades row y column
+            for (int i = 0; i < kDimension; i++)
+                for (int j = 0; j < kDimension; j++)
+                {
+                    Cells[i, j] = new Cell();
+                    Cells[i, j].Row = i;
+                    Cells[i, j].Column = j;
+                }
+
+            CurrentCell = Cells[0, 0];
+            VisitedCells = 1;
+            //limpia la pila de celdas por visitar
+            CellStack.Clear();
+        }
+
+        public void Generate()
+        {
+            while (VisitedCells < TotalCells)
+            {
+                // devuelve todos los vecinos de la celda actual dentro de los limites
+                
+                ArrayList AdjacentCells = GetNeighborsWithWalls(CurrentCell);
+                // test if a cell like this exists
+                if (AdjacentCells.Count > 0)
+                {
+                    //  elige uno de ellos, y derribar la pared entre el ramdom y la celda actual
+                    int randomCell = Cell.TheRandom.Next(0, AdjacentCells.Count);
+                    Cell theCell = ((Cell)AdjacentCells[randomCell]);
+                    CurrentCell.KnockDownWall(theCell);//derriba la pared
+                    CellStack.Push(CurrentCell); // push the current cell onto the stack
+                    CurrentCell = theCell; // make the random neighbor the new current cell
+                    VisitedCells++;
+                }
+                else
+                {
+                    // No cells with walls intact, pop current cell from stack
+                    CurrentCell = (Cell)CellStack.Pop();
+                }
+
+            }
+        }
+        public void recorrer(Cell comienzo,Cell fin) {
+            Cell celda=comienzo;
+            
+            while ( celda!= fin)
+            {
+                // get a list of the neighboring cells with all walls intact
+                ArrayList AdjacentCells = GetNeighborsWithWalls(CurrentCell);
+                // test if a cell like this exists
+                if (AdjacentCells.Count > 0)
+                {
+                    // yes, choose one of them, and knock down the wall between it and the current cell
+                    int randomCell = Cell.TheRandom.Next(0, AdjacentCells.Count);
+                    Cell theCell = ((Cell)AdjacentCells[randomCell]);
+                    CurrentCell.KnockDownWall(theCell);
+                    CellStack.Push(CurrentCell); // push the current cell onto the stack
+                    CurrentCell = theCell; // make the random neighbor the new current cell
+                    VisitedCells++;
+                }
+                else
+                {
+                    // No cells with walls intact, pop current cell from stack
+                    CurrentCell = (Cell)CellStack.Pop();
+                }
+
+            }
+        
+        
+        }
+        public void encontrarInicioFin(Graphics g)
+        {
+            int tamañoCelda = Cell.kCellSize - 1;
+            random = new Random();
+            int aleatorio;
+            Pen lapiz=Pens.Red;
+            
+            aleatorio = this.random.Next(0, kDimension-1);
+            inicio = Cells[0, aleatorio];
+            //coleccionPuntos[0]= esquina superior izq
+                g.DrawRectangle(lapiz, Cells[0, aleatorio].coleccionPuntos[0].X, Cells[0, aleatorio].coleccionPuntos[0].Y, tamañoCelda, tamañoCelda);
+                aleatorio = this.random.Next(0, kDimension-1);
+
+                fin = Cells[kDimension-1, aleatorio];
+            //coleccionPuntos[2]= esquina superior derecha
+                g.DrawRectangle(lapiz, Cells[kDimension-1, aleatorio].coleccionPuntos[2].X, Cells[kDimension-1, aleatorio].coleccionPuntos[2].Y, tamañoCelda, tamañoCelda);
+        }
+
+        public void Draw(Graphics g)
+        {
+            //construye las celdas segun su posicion
+            for (int i = 0; i < kDimension; i++)
+                for (int j = 0; j < kDimension; j++)
+                {
+                    Cells[i, j].Draw(g);
+                }
+        }
+    }
+}
---
+++ Laberinto/FormLaberinto/ModoDeJuego3.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace FormLaberinto
+{
+    partial class ModoDeJuego3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 440);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Computer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 457);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00:00:00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(598, 440);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Computer";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(598, 457);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "00:00:00";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(349, 400);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(393, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(349, 400);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ModoDeJuegoCom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 573);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ModoDeJuegoCom";
+            this.Text = "ModoDeJuegoCom";
+            this.Load += new System.EventHandler(this.ModoDeJuegoCom_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
